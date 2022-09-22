8254f1374fd09ec0bb10ce90f7d7f2ce8b9d23dd
Tony-Castro <114043633+Tony-Castro@users.noreply.github.com>
2022-09-21 22:34:57 -0700

    Add files via upload

---
+++ Call Of Duty/Form1.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(125, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Overkill Perk"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Primary"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(179, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Secondary"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 81)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Primary Attachments"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(44, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 15)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Attachment #1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(44, 120)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 15)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Attachment #2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(44, 135)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 15)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Attachment #3"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(44, 150)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(86, 15)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Attachment #4"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(179, 81)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(133, 15)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Secondary Attachments"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(200, 150)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(86, 15)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Attachment #4"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(200, 135)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(86, 15)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "Attachment #3"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(200, 120)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(86, 15)
+        Me.Label11.TabIndex = 11
+        Me.Label11.Text = "Attachment #2"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(200, 105)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(86, 15)
+        Me.Label12.TabIndex = 10
+        Me.Label12.Text = "Attachment #1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(416, 11)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Weapon Type"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(570, 19)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(81, 15)
+        Me.Label13.TabIndex = 15
+        Me.Label13.Text = " Weapon Type"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(416, 73)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 23)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Assault Rifles"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(526, 73)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 17
+        Me.Button4.Text = "SMGS"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(632, 73)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = "Shotguns"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(740, 73)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 19
+        Me.Button6.Text = "LMGS"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(839, 73)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(103, 23)
+        Me.Button7.TabIndex = 20
+        Me.Button7.Text = "Marksman Rifles"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(960, 73)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(91, 23)
+        Me.Button8.TabIndex = 21
+        Me.Button8.Text = "Sniper RIfles"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(433, 120)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(0, 15)
+        Me.Label14.TabIndex = 22
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(538, 120)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(0, 15)
+        Me.Label15.TabIndex = 23
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(643, 120)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(0, 15)
+        Me.Label16.TabIndex = 24
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(753, 120)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(0, 15)
+        Me.Label17.TabIndex = 25
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(868, 120)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(0, 15)
+        Me.Label18.TabIndex = 26
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(977, 120)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(0, 15)
+        Me.Label19.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1095, 192)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+End Class
---
+++ Call Of Duty/Form1.vb
@@ -0,0 +1,188 @@
+﻿Public Class Form1
+
+    Dim rnd As New Random
+    Dim intArray(0 To 54) As String
+
+    Dim intArray2(0 To 5) As String
+
+    Dim intArray3(0 To 12) As String
+    Dim intarray4(0 To 9) As String
+    Dim intArray5(0 To 5) As String
+    Dim intArray6(0 To 5) As String
+    Dim intArray7(0 To 5) As String
+    Dim intArray8(0 To 3) As String
+
+
+    Private Sub Form1_load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Sniper Rifles'
+        intArray8(0) = "	Dragunov	"
+        intArray8(1) = "	HDR	"
+        intArray8(2) = "	AX-50	"
+        intArray8(3) = "	Rytec AMR	"
+
+
+        'Marksman Rifles'
+        intArray7(0) = "	EBR-14	"
+        intArray7(1) = "	MK2 Carbine	"
+        intArray7(2) = "	Kar98k	"
+        intArray7(3) = "	Crossbow	"
+        intArray7(4) = "	SKS	"
+        intArray7(5) = "	SP-R 208	"
+
+
+        'LMGS'
+        intArray6(0) = "	PKM	"
+        intArray6(1) = "	SA87	"
+        intArray6(2) = "	M91	"
+        intArray6(3) = "	MG34	"
+        intArray6(4) = "	Holger-26	"
+        intArray6(5) = "	Bruen Mk9	"
+
+
+        'Shotguns'
+        intArray5(0) = "	Model 680	"
+        intArray5(1) = "	R9-0 Shotgun	"
+        intArray5(2) = "	725	"
+        intArray5(3) = "	Origin 12 Shotgun	"
+        intArray5(4) = "	VLK Rogue	"
+        intArray5(5) = "	JAK-12	"
+
+        'SMGS'
+        intarray4(0) = "	AUG	"
+        intarray4(1) = "	P90	"
+        intarray4(2) = "	MP5	"
+        intarray4(3) = "	UZI	"
+        intarray4(4) = "	PP19	"
+        intarray4(5) = "	MP7	"
+        intarray4(6) = "	STRIKER 45	"
+        intarray4(7) = "	FENNEC	"
+        intarray4(8) = "	ISO	"
+        intarray4(9) = "	CX-9	"
+
+        'Assault Rifles'
+        intArray3(0) = "Kilo 141"
+        intArray3(1) = "FAL"
+        intArray3(2) = "M4A1"
+        intArray3(3) = "FR 5.56"
+        intArray3(4) = "Oden"
+        intArray3(5) = "M13"
+        intArray3(6) = "FN Scar 17"
+        intArray3(7) = "AK 47"
+        intArray3(8) = "RAM 7"
+        intArray3(9) = "Grau 5.56"
+        intArray3(10) = "CR-56 AMAX"
+        intArray3(11) = "AN-94"
+        intArray3(12) = "AS VAL"
+
+
+        'weapon types'
+        intArray2(0) = "Assault Rifles"
+        intArray2(1) = "SMGS"
+        intArray2(2) = "Shotguns"
+        intArray2(3) = "LMGS"
+        intArray2(4) = "Marksman Rifles"
+        intArray2(5) = "Sniper Rifles"
+
+        ' all weapons'
+        intArray(0) = "	Kilo 141"
+        intArray(1) = "	FAL"
+        intArray(2) = "	M4A1"
+        intArray(3) = "	FR 5.56	"
+        intArray(4) = "	Oden"
+        intArray(5) = "	M13	"
+        intArray(6) = "	FN Scar 17	"
+        intArray(7) = "	AK 47	"
+        intArray(8) = "	RAM 7	"
+        intArray(9) = "	Grau 5.56	"
+        intArray(10) = "CR-56 AMAX	"
+        intArray(11) = "AN-94	"
+        intArray(12) = "AS VAL	"
+        intArray(13) = "AUG	"
+        intArray(14) = "P90	"
+        intArray(15) = "MP5	"
+        intArray(16) = "UZI	"
+        intArray(17) = "PP19	"
+        intArray(18) = "MP7	"
+        intArray(19) = "STRIKER 45	"
+        intArray(20) = "FENNEC	"
+        intArray(21) = "ISO	"
+        intArray(22) = "CX-9	"
+        intArray(23) = "Model 680	"
+        intArray(24) = "R9-0 Shotgun	"
+        intArray(25) = "725	"
+        intArray(26) = "Origin 12 Shotgun	"
+        intArray(27) = "VLK Rogue	"
+        intArray(28) = "JAK-12	"
+        intArray(29) = "PKM	"
+        intArray(30) = "SA87	"
+        intArray(31) = "M91	"
+        intArray(32) = "MG34	"
+        intArray(33) = "Holger-26	"
+        intArray(34) = "Bruen Mk9	"
+        intArray(35) = "EBR-14	"
+        intArray(36) = "MK2 Carbine	"
+        intArray(37) = "Kar98k	"
+        intArray(38) = "Crossbow	"
+        intArray(39) = "SKS	"
+        intArray(40) = "SP-R 208	"
+        intArray(41) = "Dragunov	"
+        intArray(42) = "HDR	"
+        intArray(43) = "AX-50	"
+        intArray(44) = "Rytec AMR	"
+        intArray(45) = "Riot Shield	"
+        ' inserting different array variables, from 46, X
+        intArray(46) = "Muzzle"
+        intArray(47) = "Barrel"
+        intArray(48) = "Laser"
+        intArray(49) = "Optic"
+        intArray(50) = "Stock"
+        intArray(51) = "Underbarrel"
+        intArray(52) = "Magazine"
+        intArray(53) = "Rear Grip"
+        intArray(54) = "Perk"
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = intArray(rnd.Next(0, 46))
+        Label2.Text = intArray(rnd.Next(0, 46))
+        Label4.Text = intArray(rnd.Next(46, 49))
+        Label5.Text = intArray(rnd.Next(49, 51))
+        Label6.Text = intArray(rnd.Next(51, 53))
+        Label7.Text = intArray(rnd.Next(53, 55))
+        Label9.Text = intArray(rnd.Next(46, 49))
+        Label10.Text = intArray(rnd.Next(49, 51))
+        Label11.Text = intArray(rnd.Next(51, 53))
+        Label12.Text = intArray(rnd.Next(53, 55))
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label13.Text = intArray2(rnd.Next(0, 6))
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label14.Text = intArray3(rnd.Next(0, 13))
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label15.Text = intarray4(rnd.Next(0, 10))
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Label16.Text = intArray5(rnd.Next(0, 6))
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Label17.Text = intArray6(rnd.Next(0, 6))
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Label18.Text = intArray7(rnd.Next(0, 6))
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Label19.Text = intArray8(rnd.Next(0, 4))
+    End Sub
+End Class
